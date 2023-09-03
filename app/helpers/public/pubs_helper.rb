026e60d5bd86b6721dabb8bdc1f809128e3f7f43
EC2 Default User <ec2-user@ip-172-31-14-123.ap-northeast-1.compute.internal>
2023-09-03 14:54:50 +0000

    update

---
+++ app/helpers/public/pubs_helper.rb
@@ -0,0 +1,2 @@
+module Public::PubsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+scope module: :public do
+    root to: 'homes#top'
+    resources :pub, only: [:new, :index, :update, :create, :edit, :destroy]
+    get "users/confirmation" => 'users#confirmation'
+    patch "users/withdrawal" => 'userrs#withdrawal'
+    resources :users, only: [:index, :show, :edit, :update]
+end
+
+namespace :admin do
+   root to: 'homes#top'
+   resources :users, only: [:index, :show, :edit, :update]
+end
+
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
